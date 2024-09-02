7edc46ad8300468502167fcb439a625e00d43dad
KingAceBlack <119745116+KingAceBlack@users.noreply.github.com>
2024-09-01 19:48:38 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,8 +22,8 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/dtech.png"
-              alt="dTech Logo"
+              src="/pepe.jpg"
+              alt="cool pepe"
               // className="dark:invert"
               width={100}
               height={24}
@@ -36,8 +36,8 @@ export default function Home() {
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/dtech.png"
-          alt="dTech Logo"
+          src="/pepe.jpg"
+          alt="cool pepe"
           width={180}
           height={37}
           priority
@@ -56,7 +56,7 @@ export default function Home() {
               type: "createCast",
               data: {
                 cast: {
-                  text: "dTech is the best!",
+                  text: "Diamond Hands!",
                   embeds: ["https://dtech.vision"]
                 }
               }
