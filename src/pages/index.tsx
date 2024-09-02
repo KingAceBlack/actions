01397fe33aa305542ad4fdaba95b3a3b4a86b29f
KingAceBlack <119745116+KingAceBlack@users.noreply.github.com>
2024-09-01 19:59:29 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           uwu&nbsp;
-          <code className="font-mono font-bold">@samuellhuber</code>
+          <code className="font-mono font-bold">lorecaster</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
@@ -47,7 +47,7 @@ export default function Home() {
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <p>
-          Hello main test?
+          What's Next?
         </p>
         <button 
           className="px-4 py-2 text-white bg-blue-500 rounded-md shadow-md hover:bg-blue-600"
